d142128abd20f425bd8b407be6101b4a240d861b
Lue Hang <luehang17@gmail.com>
2020-01-05 01:02:34 -0600

    Remove & remove componentWillMount.

--- src/index.js
+++ src/index.js
@@ -113,7 +113,7 @@ export default class CameraRollSelector extends React.PureComponent {
         };
     }
 
-    componentWillMount = async () => {
+    componentDidMount = async () => {
         if (this.props.enableCameraRoll) {
             if (Platform.OS === "android") {
                 const granted = await PermissionsAndroid.request(
