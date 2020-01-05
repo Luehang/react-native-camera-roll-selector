edd2c7bc3d272ad87c3df0bcbba7fa06362046ec
Lue Hang <luehang17@gmail.com>
2020-01-05 00:59:11 -0600

    Update example to React Native 0.61.

--- example/ReactNativeCameraRollSelectorExample.js
+++ example/ReactNativeCameraRollSelectorExample.js
@@ -8,17 +8,181 @@ import {
     Text,
     Image,
     TouchableWithoutFeedback,
-    Modal,
-    Slider,
-    Switch
+    // Modal,
+    // Slider,
+    // Switch
 } from "react-native";
 import CameraRollSelector from "react-native-camera-roll-selector";
+// import CameraRollSelector from "./src";
 
 const deviceHeight = Dimensions.get("window").height;
 const deviceWidth = Dimensions.get("window").width;
 const platform = Platform.OS;
 
 const settingsIcon = require("./assets/outline_settings_white_36pt_2x.png");
+
+export default class ReactNativeCameraRollSelectorExample extends Component {
+    state = {
+        isSettings: true,
+        imagesPerRow: 2,
+        spacing: 2,
+        isContainerWidth: false,
+        containerWidth: undefined,
+        statusBarPaddingTop: isIPhoneX() ? 30 : platform === "ios" ? 20 : 0
+    }
+
+    onLayoutChange = (ev) => {
+        const { width, height } = ev.nativeEvent.layout;
+        let maxComp = Math.max(width, height);
+
+        if (width >= maxComp) {
+            this.setState({
+                statusBarPaddingTop: 0
+            });
+        } else if (width < maxComp) {
+            this.setState({
+                statusBarPaddingTop: isIPhoneX() ? 30 : platform === "ios" ? 20 : 0
+            });
+        }
+    }
+
+    render() {
+        const {
+            // isSettings,
+            imagesPerRow,
+            spacing,
+            isContainerWidth,
+            containerWidth,
+            statusBarPaddingTop
+        } = this.state;
+
+        return (
+            <View
+                onLayout={(ev) => this.onLayoutChange(ev)}
+                style={styles.container}
+            >
+                <View style={[styles.header, styles.mobileHeader, { paddingTop: statusBarPaddingTop }]}>
+                    <Image
+                        source={{ uri: "https://luehangs.site/images/lue-hang2018-square.jpg" }}
+                        style={{height: 35, width: 35, marginLeft: 10, borderRadius: 20}} />
+                    <View style={styles.headerBody}>
+                        <Text style={styles.title}>CameraRollSelector</Text>
+                    </View>
+                    <TouchableWithoutFeedback
+                        onPress={() => {}}>
+                        <Image
+                            source={settingsIcon}
+                            style={{height: 35, width: 35, marginRight: 10}}
+                        />
+                    </TouchableWithoutFeedback>
+                </View>
+                <View style={styles.listTab}>
+                    <TouchableWithoutFeedback
+                        style={{borderTopLeftRadius: 7.5,}}
+                        onPress={() => Linking.openURL("https://luehangs.site")}>
+                            <View style={styles.tab}>
+                                <View style={{paddingBottom: 3}}>
+                                    <Text style={styles.tabTextOff}>REMOTE</Text>
+                                </View>
+                            </View>
+                    </TouchableWithoutFeedback>
+                    <TouchableWithoutFeedback>
+                        <View style={styles.tab}>
+                            <View style={[styles.tabTextUnderline, {paddingBottom: 3}]}>
+                                <Text style={styles.tabTextOn}>CAMERA ROLL</Text>
+                            </View>
+                        </View>
+                    </TouchableWithoutFeedback>
+                </View>
+                <CameraRollSelector
+                    imagesPerRow={imagesPerRow}
+                    spacing={spacing}
+                    containerWidth={isContainerWidth ? containerWidth : undefined}
+                />
+                {/* <CameraRollSelector
+                    imagesPerRow={imagesPerRow}
+                    spacing={spacing}
+                    containerWidth={isContainerWidth ? containerWidth : undefined}
+                    enableCameraRoll={false}
+                    onGetData={() => {
+                        return {
+                            assets: [
+                                // Can be used with different image object fieldnames.
+                                // Ex. source, source.uri, uri, URI, url, URL
+                                { uri: "https://luehangs.site/pic-chat-app-images/beautiful-blond-blonde-hair-478544.jpg" },
+                                // { source: require("yourApp/image.png"),
+                                //     // IMPORTANT: It is REQUIRED for LOCAL IMAGES
+                                //     // to include a dimensions field with the
+                                //     // actual width and height of the image or
+                                //     // it will throw an error.
+                                //     dimensions: { width: 1080, height: 1920 } },
+                                { source: { uri: "https://luehangs.site/pic-chat-app-images/beautiful-beautiful-women-beauty-40901.jpg" } },
+                                { uri: "https://luehangs.site/pic-chat-app-images/animals-avian-beach-760984.jpg" },
+                                { URI: "https://luehangs.site/pic-chat-app-images/beautiful-blond-fishnet-stockings-48134.jpg" },
+                                { url: "https://luehangs.site/pic-chat-app-images/beautiful-beautiful-woman-beauty-9763.jpg" },
+                                { URL: "https://luehangs.site/pic-chat-app-images/attractive-balance-beautiful-186263.jpg" },
+                            ],
+                            pageInfo: {
+                                hasNextPage: false
+                            }
+                        };
+                    }}
+                /> */}
+                {/* <Modal animationType="fade"
+                    transparent={true}
+                    visible={isSettings}
+                    presentationStyle="overFullScreen"
+                    style={{ paddingTop: statusBarPaddingTop }}>
+                    <View style={styles.horizontal}>
+                        <Text style={{fontSize: 20, color: "#0277bd", fontWeight: "bold"}}>imagesPerRow</Text>
+                        <Slider
+                            style={{ width: 300 }}
+                            step={1}
+                            minimumValue={2}
+                            maximumValue={7}
+                            value={imagesPerRow}
+                            onValueChange={val => this.setState({ imagesPerRow: val })}
+                        />
+                        <Text style={{fontSize: 25, color: "#009688", fontWeight: "bold", alignSelf: "flex-end"}}>
+                            {imagesPerRow}
+                        </Text>
+                        <Text style={{fontSize: 20, color: "#0277bd", fontWeight: "bold"}}>spacing</Text>
+                        <Slider
+                            style={{ width: 300 }}
+                            step={0.5}
+                            minimumValue={1}
+                            maximumValue={5}
+                            value={spacing}
+                            onValueChange={val => this.setState({ spacing: val })}
+                        />
+                        <Text style={{fontSize: 25, color: "#009688", fontWeight: "bold", alignSelf: "flex-end"}}>
+                            {spacing}
+                        </Text>
+                        <Text style={{fontSize: 20, color: "#0277bd", fontWeight: "bold"}}>containerWidth</Text>
+                        <View style={{flexDirection: "row", marginVertical: 5}}>
+                            <Text style={{flex: 1, fontSize: 20, color: "#0277bd", fontWeight: "bold", textAlign: "left"}}>enable</Text>
+                            <Switch
+                                onValueChange={(val) => this.setState({ isContainerWidth: val })}
+                                value={isContainerWidth}
+                            />
+                        </View>
+                        <Slider
+                            style={{ width: 300 }}
+                            step={50}
+                            minimumValue={50}
+                            maximumValue={deviceWidth}
+                            value={containerWidth}
+                            onValueChange={val => this.setState({ containerWidth: val })}
+                        />
+                        <Text style={{fontSize: 25, color: "#009688", fontWeight: "bold", alignSelf: "flex-end"}}>
+                            {containerWidth ? containerWidth : "undefined"}
+                        </Text>
+                    </View>
+                </Modal> */}
+            </View>
+        );
+    }
+}
 
 function isIPhoneX() {
     const X_WIDTH = 375;
@@ -77,146 +241,12 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 25
     },
-    horizontal: {
-        flex: 1,
-        backgroundColor: "rgba(0,0,0,0.65)",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: 30
-    }
+    // horizontal: {
+    //     flex: 1,
+    //     backgroundColor: "rgba(0,0,0,0.65)",
+    //     justifyContent: "center",
+    //     flexDirection: "column",
+    //     alignItems: "center",
+    //     padding: 30
+    // }
 });
-
-export default class ReactNativeCameraRollSelectorExample extends Component {
-    state = {
-        isSettings: true,
-        imagesPerRow: 2,
-        spacing: 2,
-        isContainerWidth: false,
-        containerWidth: undefined,
-        statusBarPaddingTop: isIPhoneX() ? 30 : platform === "ios" ? 20 : 0
-    }
-
-    onLayoutChange = (ev) => {
-        const { width, height } = ev.nativeEvent.layout;
-        let maxComp = Math.max(width, height);
-
-        if (width >= maxComp) {
-            this.setState({
-                statusBarPaddingTop: 0
-            });
-        } else if (width < maxComp) {
-            this.setState({
-                statusBarPaddingTop: isIPhoneX() ? 30 : platform === "ios" ? 20 : 0
-            });
-        }
-    }
-
-    render() {
-        const {
-            isSettings,
-            imagesPerRow,
-            spacing,
-            isContainerWidth,
-            containerWidth,
-            statusBarPaddingTop
-        } = this.state;
-
-        return (
-            <View
-                onLayout={(ev) => this.onLayoutChange(ev)}
-                style={styles.container}
-            >
-                <View style={[styles.header, styles.mobileHeader, { paddingTop: statusBarPaddingTop }]}>
-                    <Image
-                        source={{ uri: "https://luehangs.site/images/lue-hang2018-square.jpg" }}
-                        style={{height: 35, width: 35, marginLeft: 10, borderRadius: 20}} />
-                    <View style={styles.headerBody}>
-                        <Text style={styles.title}>CameraRollSelector</Text>
-                    </View>
-                    <TouchableWithoutFeedback
-                        onPress={() => {}}>
-                        <Image
-                            source={settingsIcon}
-                            style={{height: 35, width: 35, marginRight: 10}}
-                        />
-                    </TouchableWithoutFeedback>
-                </View>
-                <View style={styles.listTab}>
-                    <TouchableWithoutFeedback
-                        style={{borderTopLeftRadius: 7.5,}}
-                        onPress={() => Linking.openURL("https://luehangs.site")}>
-                            <View style={styles.tab}>
-                                <View style={{paddingBottom: 3}}>
-                                    <Text style={styles.tabTextOff}>REMOTE</Text>
-                                </View>
-                            </View>
-                    </TouchableWithoutFeedback>
-                    <TouchableWithoutFeedback>
-                        <View style={styles.tab}>
-                            <View style={[styles.tabTextUnderline, {paddingBottom: 3}]}>
-                                <Text style={styles.tabTextOn}>CAMERA ROLL</Text>
-                            </View>
-                        </View>
-                    </TouchableWithoutFeedback>
-                </View>
-                <CameraRollSelector
-                    imagesPerRow={imagesPerRow}
-                    spacing={spacing}
-                    containerWidth={isContainerWidth ? containerWidth : undefined}
-                />
-                {/* <Modal animationType="fade"
-                    transparent={true}
-                    visible={isSettings}
-                    presentationStyle="overFullScreen"
-                    style={{ paddingTop: statusBarPaddingTop }}>
-                    <View style={styles.horizontal}>
-                        <Text style={{fontSize: 20, color: "#0277bd", fontWeight: "bold"}}>imagesPerRow</Text>
-                        <Slider
-                            style={{ width: 300 }}
-                            step={1}
-                            minimumValue={2}
-                            maximumValue={7}
-                            value={imagesPerRow}
-                            onValueChange={val => this.setState({ imagesPerRow: val })}
-                        />
-                        <Text style={{fontSize: 25, color: "#009688", fontWeight: "bold", alignSelf: "flex-end"}}>
-                            {imagesPerRow}
-                        </Text>
-                        <Text style={{fontSize: 20, color: "#0277bd", fontWeight: "bold"}}>spacing</Text>
-                        <Slider
-                            style={{ width: 300 }}
-                            step={0.5}
-                            minimumValue={1}
-                            maximumValue={5}
-                            value={spacing}
-                            onValueChange={val => this.setState({ spacing: val })}
-                        />
-                        <Text style={{fontSize: 25, color: "#009688", fontWeight: "bold", alignSelf: "flex-end"}}>
-                            {spacing}
-                        </Text>
-                        <Text style={{fontSize: 20, color: "#0277bd", fontWeight: "bold"}}>containerWidth</Text>
-                        <View style={{flexDirection: "row", marginVertical: 5}}>
-                            <Text style={{flex: 1, fontSize: 20, color: "#0277bd", fontWeight: "bold", textAlign: "left"}}>enable</Text>
-                            <Switch
-                                onValueChange={(val) => this.setState({ isContainerWidth: val })}
-                                value={isContainerWidth}
-                            />
-                        </View>
-                        <Slider
-                            style={{ width: 300 }}
-                            step={50}
-                            minimumValue={50}
-                            maximumValue={deviceWidth}
-                            value={containerWidth}
-                            onValueChange={val => this.setState({ containerWidth: val })}
-                        />
-                        <Text style={{fontSize: 25, color: "#009688", fontWeight: "bold", alignSelf: "flex-end"}}>
-                            {containerWidth ? containerWidth : "undefined"}
-                        </Text>
-                    </View>
-                </Modal> */}
-            </View>
-        );
-    }
-}
